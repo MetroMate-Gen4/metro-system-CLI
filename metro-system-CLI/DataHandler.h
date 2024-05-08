f757aee029d0f142010befe56b32705b3aac91eb
IB <ibrahemelyamany2004@gmail.com>
2024-05-08 21:07:34 +0300

    add sign in / sign up (IB)

--- metro-system-CLI/DataHandler.h
+++ metro-system-CLI/DataHandler.h
@@ -4,16 +4,20 @@
 #include <stack>
 #include "User.h"
 #include "Line.h"
+#include "Admin.h"
+
 class DataHandler
 {
 public:
+    Admin* admin = new Admin("admin", "admin");
     static std::unordered_map<int, User*>users;
     static std::stack<Ride> rides;
     stack<User*> undoStackUser;
     DataHandler();
     ~DataHandler();
     User* searchUser(int id);
-    void addUser(User newUser);
+    User* searchUserEmail(std::string email);
+    void addUser(User* newUser);
     void editUserEmail(int userId, std::string newEmail);
     void editUserPassword(int userId, std::string newPassword);
     void editUserName(int userId, std::string newName);
@@ -26,8 +30,15 @@ public:
     void deleteLine(int lineId);
     void undoEditUser();
     void mainCLI();
+    std::string choice();
+    void displayMainMenu_SignIn_SignUp();
+    Account* displaySignIn();
+    void displaySignUp();
+    void displayHomeUser(User* u);
+    void displayHomeAdmin(Admin* a);
+    void displayAdminDashboard(Admin* a);
     User* logIn(std::string email, std::string pass);
-    void signUp(std::string email, std::string password, std::string name, int nationalId, int age);
+    bool signUp(std::string email, std::string password, std::string name, int nationalId, int age);
     void displayRidesCLI(User* user);
     void displayAllRidesCLI();
 };
--- metro-system-CLI/main.cpp
+++ metro-system-CLI/main.cpp
@@ -6,25 +6,27 @@
 using namespace std;
 
 int main() {
-	User* u = new User("aa", "123");
-	User* u2 = new User("IB", "18", "Ibrahem", 0, 0);
-	u->setName("Cellz");
-	Station* s1 = new Station();
-	s1->setName("TEST1");
-	Station* s2 = new Station();
-	s2->setName("TEST2");
-	Ride* r = new Ride();
-	Ride* r2 = new Ride();
-	r->setStartingStation(*s1);
-	r->setEndingStation(*s2);
-	r2->setStartingStation(*s2);
-	r2->setEndingStation(*s1);
-	u->addRide(*r);
-	/*u->addRide(*r);
-	u->addRide(*r);*/
-	u2->addRide(*r2);
+	//User* u = new User("aa", "123");
+	//User* u2 = new User("IB", "18", "Ibrahem", 0, 0);
+	//u->setName("Cellz");
+	//Station* s1 = new Station();
+	//s1->setName("TEST1");
+	//Station* s2 = new Station();
+	//s2->setName("TEST2");
+	//Ride* r = new Ride();
+	//Ride* r2 = new Ride();
+	//r->setStartingStation(*s1);
+	//r->setEndingStation(*s2);
+	//r2->setStartingStation(*s2);
+	//r2->setEndingStation(*s1);
+	//u->addRide(*r);
+	///*u->addRide(*r);
+	//u->addRide(*r);*/
+	//u2->addRide(*r2);
+	//DataHandler d;
+	//d.displayRidesCLI(u);
+	//d.displayRidesCLI(u2);
+	//d.displayAllRidesCLI();
 	DataHandler d;
-	d.displayRidesCLI(u);
-	d.displayRidesCLI(u2);
-	d.displayAllRidesCLI();
+	d.mainCLI();
 }
