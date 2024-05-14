eae9a0c9533b4789a065c38aa6ce6d7119fbe4a0
Eslam adly <eslamadly666@gmail.com>
2024-05-14 23:46:14 +0300

    fix

--- metro-system-CLI/User.cpp
+++ metro-system-CLI/User.cpp
@@ -6,14 +6,11 @@
 using namespace std;
 
 
-User::User(string email, string password) : Account(email, password), checkedInStationId(-1), usedTicket(-1),wallet(0)
-User::User() :Account() {
+User::User(string email, string password) : Account(email, password), checkedInStationId(-1), usedTicket(-1),wallet(0){}
+User::User() :Account(), wallet(0) {
 
 }
 
-User::User(string email, string password) : Account(email, password), checkedInStationId(-1), usedTicket(-1)
-{
-}
 
 User::User(std::string email, std::string password, std::string name, int nationalId, int age) :Account(email, password, name, nationalId, age), checkedInStationId(-1), usedTicket(-1), wallet(0) {
 
